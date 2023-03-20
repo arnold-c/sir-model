98c996665b5687a3ab4a61e7a767c87d2236b87c
Callum <cfa5228@psu.edu>
2023-03-20 17:26:25 -0400

    Add final size calculation code

--- src/simulation.R
+++ src/simulation.R
@@ -1,5 +1,6 @@
 library(deSolve)
 library(tidyverse)
+library(rootSolve)
 
 theme_set(theme_minimal())
 
@@ -43,3 +44,32 @@ out_df <- as_tibble(out) %>%
 ggplot(out_df, aes(x = time, y = number, color = state)) +
   geom_line(linewidth = 2) +
   labs(x = "Time", y = "Number", color = "State")
+
+
+# Candidate values for R0 and beta
+R0 <- seq(0.1, 5, length = 50)
+betas <- R0 * 1/2
+
+# Calculate proportion infected for each value of R0
+# map2_dfr is a {purrr} function that applies a function to two vectors i.e., it is a vectorized version of a for loop, and returns a data frame
+final_size_df <- map2_dfr(
+  .x = betas,
+  .y = R0,
+  .f = function(.x, .y) {
+    equil <- runsteady(
+      y = c(S = 1 - 1E-5, I = 1E-5, R = 0),
+      times = c(0, 1E5),
+      func = sirmod,
+      parms = c(mu = 0, N = 1, beta = .x, gamma = 1/2)
+    )
+    
+    tibble(
+      R0 = .y,
+      final_size = equil$y["R"]
+    )
+  }
+)
+
+ggplot(final_size_df, aes(x = R0, y = final_size)) +
+  geom_line(linewidth = 2) +
+  labs(x = "R0", y = "Final size")
